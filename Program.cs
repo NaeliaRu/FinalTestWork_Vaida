0d174eb7eb4829413bd4237775a3401ce62ffb30
Natalya Vaida <naelia.ru@gmail.com>
2022-11-26 21:34:31 +0300

    user array creation and added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,33 @@
-﻿
+﻿/* Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+лучше обойтись исключительно массивами.
+
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → [] */
+
+string[] CreateArray()
+{
+    Console.Write("Input array size: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[size];
+
+    Console.Write("Create a new array");
+
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Input array element {i + 1}: ");
+        array[i] = Console.ReadLine();
+    }
+    Console.Write("Your array: ");
+    return array;
+}
+
+
+
+
 
 
 
